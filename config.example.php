df7d01dcc867f5e51657b762c222332a9b33a08a
Koen Hendriks <info@koenhendriks.com>
2014-12-09 12:27:53 +0100

    No longer need api key

--- config.example.php
+++ config.example.php
@@ -19,6 +19,3 @@ $channels = array('#tesingmybot');
 //Admin Settings
 define('ADMINNICK', 'Chameleon');
 define('ADMINPASS', 'habbo123');
-
-//Whoapi.com settings
-define('WHOIS_KEY', 'ab123cd4e5fgh6i78j9k012lm3n456o7');
